e10c1dd266e73388355b9d8532961dd57259a184
Nicolas <nicolas.baudvin@gmail.com>
2020-01-23 18:52:51 +0100

    responsive static homepage

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './home.scss';
+import { Link } from 'react-router-dom';
 
 export default () => {
+
   return (
     <main className="homepage">
-        body
+      <div className="title-container">
+        <h1 className="homepage-title">See My Games</h1>
+        <aside className="homepage-title-aside"><div className="title-text">beta</div></aside>
+      </div>
+      <button type="button" className="homepage-button"><Link to="/inscription"> commencer </Link></button>
     </main>
   );
 };
