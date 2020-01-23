46ab15a11cd89e4b66264afed4f95e9edf52e687
Nicolas <nicolas.baudvin@gmail.com>
2020-01-23 21:05:06 +0100

    add tooltip

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -4,13 +4,39 @@ import { Link } from 'react-router-dom';
 
 export default () => {
 
+  const followCursor = (target) => (e) => {
+    const buttonTooltip = document.querySelector('.button-tooltip');
+    switch (target) {
+      case "button": {
+        buttonTooltip.style.display = "inline";
+        buttonTooltip.style.left = `${e.pageX + 10}px`;
+        buttonTooltip.style.top = `${e.pageY + 10}px`;
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
+  const hideTooltip = () => {
+    const buttonTooltip = document.querySelector('.button-tooltip');
+    buttonTooltip.style.display = "none";
+  };
+
   return (
     <main className="homepage">
       <div className="title-container">
         <h1 className="homepage-title">See My Games</h1>
         <aside className="homepage-title-aside"><div className="title-text">beta</div></aside>
       </div>
-      <button type="button" className="homepage-button"><Link to="/inscription"> commencer </Link></button>
+      <span className="button-tooltip">Redirection vers la page inscription</span>
+      <button onMouseLeave={hideTooltip} onMouseMove={followCursor("button")} type="button" className="homepage-button">
+        <Link to="/inscription" className="button-link">
+          commencer
+        </Link>
+      </button>
+
     </main>
   );
 };
