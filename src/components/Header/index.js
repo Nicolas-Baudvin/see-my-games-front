8c8669cfa3adcbd3a4876bffedfa0093d19227f3
Nicolas <nicolas.baudvin@gmail.com>
2020-01-23 21:04:48 +0100

    add tooltips

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -22,6 +22,10 @@ export default () => {
         if (item.title === navSelected) {
           item.isSelected = false;
         }
+        if (name === "home") {
+          setNav(headerNav);
+          setSelected('');
+        }
       }
       return item;
     });
@@ -29,10 +33,56 @@ export default () => {
     setSelected(name);
   };
 
+  const followCursor = (target) => (e) => {
+    const tooltip = document.querySelector('.logo-tooltip');
+    const headerTltip = document.querySelectorAll('.header-tooltip');
+    switch (target) {
+      case "logo": {
+        tooltip.style.left = `${e.pageX + 5}px`;
+        tooltip.style.top = `${e.pageY + 5}px`;
+        break;
+      }
+      case "articles": {
+        headerTltip[0].style.left = `${e.pageX + 5}px`;
+        headerTltip[0].style.top = `${e.pageY + 5}px`;
+        break;
+      }
+      case "forum": {
+        headerTltip[1].style.left = `${e.pageX + 5}px`;
+        headerTltip[1].style.top = `${e.pageY + 5}px`;
+        break;
+      }
+      case "connexion": {
+        headerTltip[2].style.left = `${e.pageX + 5}px`;
+        headerTltip[2].style.top = `${e.pageY + 5}px`;
+        break;
+      }
+      case "inscription": {
+        headerTltip[3].style.left = `${e.pageX + 5}px`;
+        headerTltip[3].style.top = `${e.pageY + 5}px`;
+        break;
+      }
+      case "mes jeux": {
+        headerTltip[4].style.left = `${e.pageX + 5}px`;
+        headerTltip[4].style.top = `${e.pageY + 5}px`;
+        break;
+      }
+      case "mon compte": {
+        headerTltip[5].style.left = `${e.pageX + 5}px`;
+        headerTltip[5].style.top = `${e.pageY + 5}px`;
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
   return (
     <header className="header">
-      <Link to="/" className="logo">
-        <img src="/src/assets/logo-smg-transparent.png" alt="Logo du site" />
+      <Link onClick={handleLinkClick("home")} to="/" className="logo">
+        <img onMouseMove={followCursor("logo")} src="/src/assets/logo-smg-transparent.png" alt="Logo du site" />
+        <span className="logo-tooltip">Retour page d'acceuil</span>
       </Link>
       <div className="responsive">
         <Icon onClick={() => setVisible(!visible)} className="responsive-icon" name="bars" size="big" color="" />
@@ -47,7 +97,7 @@ export default () => {
         visible={visible}
         width="thin"
       >
-        <Menu.Item as="a">
+        <Menu.Item as="div">
           <Link to="/" className="logo">
             <img src="/src/assets/logo-smg-transparent.png" alt="Logo du site" />
           </Link>
@@ -55,7 +105,7 @@ export default () => {
         {
           nav.map((item) => {
             if (item.title === "connexion" || item.title === "inscription") {
-              return !isConnected && <Menu.Item as="div">
+              return !isConnected && <Menu.Item key={item.title} as="div">
                 <Link to={item.path} className="sidemenu-item">
                   <Icon name={item.title === "connexion" ? "user" : "user plus"} size="big" />
                   <h3 className="sidemenu-title">{item.title}</h3>
@@ -64,7 +114,7 @@ export default () => {
             }
 
             if (item.title === "mon compte" || item.title === "mes jeux") {
-              return isConnected && <Menu.Item as="div">
+              return isConnected && <Menu.Item key={item.title} as="div">
                 <Link to={item.path} className="sidemenu-item">
                   <Icon name={item.title === "mon compte" ? "address book" : "gamepad"} size="big" />
                   <h3 className="sidemenu-title">{item.title}</h3>
@@ -72,7 +122,7 @@ export default () => {
               </Menu.Item>;
             }
 
-            return <Menu.Item as="div">
+            return <Menu.Item key={item.title} as="div">
               <Link to={item.path} className="sidemenu-item">
                 <Icon name={item.title === "articles" ? "newspaper" : "chat"} size="big" />
                 <h3 className="sidemenu-title">{item.title}</h3>
@@ -89,33 +139,39 @@ export default () => {
             nav.map((item) => {
               if (item.title === "connexion" || item.title === "inscription") {
                 return !isConnected && <Link
+                  onMouseMove={followCursor(item.title)}
                   key={item.title}
                   onClick={handleLinkClick(item.title)}
                   to={item.path}
                   className={ClassNames('header-nav__ul--item', { selected: item.isSelected })}
                 >
                   {item.title}
+                  <span className="header-tooltip">Page {item.title}</span>
                 </Link>;
               }
 
               if (item.title === "mon compte" || item.title === "mes jeux") {
                 return isConnected && <Link
+                  onMouseMove={followCursor(item.title)}
                   key={item.title}
                   onClick={handleLinkClick(item.title)}
                   to={item.path}
                   className={ClassNames('header-nav__ul--item', { selected: item.isSelected })}
                 >
                   {item.title}
+                  <span className="header-tooltip">Page {item.title} </span>
                 </Link>;
               }
 
               return <Link
+                onMouseMove={followCursor(item.title)}
                 key={item.title}
                 onClick={handleLinkClick(item.title)}
                 to={item.path}
                 className={ClassNames('header-nav__ul--item', { selected: item.isSelected })}
               >
                 {item.title}
+                <span className="header-tooltip">Page {item.title} </span>
               </Link>;
             })
           }
