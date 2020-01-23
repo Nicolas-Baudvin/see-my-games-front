ca7349aeb63eaf7c609f93a3a17136cd23a5e4ad
Nicolas <nicolas.baudvin@gmail.com>
2020-01-23 18:53:05 +0100

    add others nav

--- src/data/navs.js
+++ src/data/navs.js
@@ -1,10 +1,26 @@
 export const headerNav = [
-  { title: "articles", isSelected: false, path: "articles" },
-  { title: "forum", isSelected: false, path: "forum" },
-  { title: "mon compte", isSelected: false, path: "mon-compte" },
-  { title: "mes jeux", isSelected: false, path: "mes-jeux" },
-  { title: "connexion", isSelected: false, path: "connexion" },
-  { title: "inscription", isSelected: false, path: "inscription" },
+  { title: "articles", isSelected: false, path: "articles/" },
+  { title: "forum", isSelected: false, path: "forum/" },
+  { title: "mon compte", isSelected: false, path: "mon-compte/" },
+  { title: "mes jeux", isSelected: false, path: "mes-jeux/" },
+  { title: "connexion", isSelected: false, path: "connexion/" },
+  { title: "inscription", isSelected: false, path: "inscription/" },
 ];
 
-export const footerNav = [];
+export const sitePlanNav = [
+  { title: "Plan du site", isSelected: false, path: "plan-du-site/" },
+  { title: "Besoin d'aide ?", isSelected: false, path: "aide/" },
+  { title: "Sitemap", isSelected: false, path: "sitemap/" },
+];
+
+export const aproposNav = [
+  { title: "Assitance", isSelected: false, path: "assitance/" },
+  { title: "Qui sommes nous ?", isSelected: false, path: "a-propos/" },
+  { title: "Charte de confidentialité", isSelected: false, path: "charte-confidentialite/" },
+  { title: "Conditions Générales d'Utilisation", isSelected: false, path: "cgu/" },
+];
+
+export const legalsNav = [
+  { title: "Mentions légales", isSelected: false, path: "mentions-legales/" },
+  { title: "Informations légales", isSelected: false, path: "informations-legales/" },
+];
