830300994dd0808acd1ad99df1a2cb7d7f5cbccb
Nicolas <nicolas.baudvin@gmail.com>
2020-01-24 15:19:14 +0100

    WIP: Static Signup component

---
+++ src/components/Signup/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import './signup.scss';
+
+const Signup = () => {
+  return (
+    <div className="signup">
+
+    </div>
+  );
+};
+
+export default Signup;
