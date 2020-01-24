1b6ab2028e10c598a4536503d145a5122947d377
Nicolas <nicolas.baudvin@gmail.com>
2020-01-24 15:18:23 +0100

    fix path names

--- src/data/navs.js
+++ src/data/navs.js
@@ -1,10 +1,10 @@
 export const headerNav = [
-  { title: "articles", isSelected: false, path: "articles/" },
-  { title: "forum", isSelected: false, path: "forum/" },
-  { title: "mon compte", isSelected: false, path: "mon-compte/" },
-  { title: "mes jeux", isSelected: false, path: "mes-jeux/" },
-  { title: "connexion", isSelected: false, path: "connexion/" },
-  { title: "inscription", isSelected: false, path: "inscription/" },
+  { title: "articles", isSelected: false, path: "/articles/" },
+  { title: "forum", isSelected: false, path: "/forum/" },
+  { title: "mon compte", isSelected: false, path: "/mon-compte/" },
+  { title: "mes jeux", isSelected: false, path: "/mes-jeux/" },
+  { title: "connexion", isSelected: false, path: "/connexion/" },
+  { title: "inscription", isSelected: false, path: "/inscription/" },
 ];
 
 export const sitePlanNav = [
