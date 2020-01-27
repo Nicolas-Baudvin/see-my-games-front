f342d17e7727c9b7ee6ee32a82297c58c7fb0881
Nicolas <nicolas.baudvin@gmail.com>
2020-01-27 17:52:40 +0100

    WIP: account page

---
+++ src/components/Account/changeEmail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Input, Button, Icon } from 'semantic-ui-react';
+import { useSelector, useDispatch } from 'react-redux';
+
+
+export default () => {
+  return (
+    <div className="emailchange">
+      
+    </div>
+  );
+};
