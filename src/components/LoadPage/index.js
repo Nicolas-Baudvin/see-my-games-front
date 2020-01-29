4b5e750a07c2824ff9bb13e40ab3ea6b5df9f0b2
Nicolas <nicolas.baudvin@gmail.com>
2020-01-29 18:44:51 +0100

    WIP: Load page

---
+++ src/components/LoadPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+const LoadPage = () => {
+  return (
+    <div className="loadpage">
+    chargement
+    </div>
+  );
+};
+
+export default LoadPage;
