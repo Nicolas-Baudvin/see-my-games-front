c25436fffe562aa58d43fa901d6993bba33e6111
Nicolas <nicolas.baudvin@gmail.com>
2020-01-30 17:35:35 +0100

    static LoadPage

--- src/components/LoadPage/index.js
+++ src/components/LoadPage/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import { Icon } from 'semantic-ui-react';
+
+import './load.scss';
 
 const LoadPage = () => {
   return (
     <div className="loadpage">
-    chargement
+      <Icon size="huge" className="loadpage-icon" loading name='spinner' />
+      <h2 className="loadpage-message">Chargement ...</h2>
     </div>
   );
 };
