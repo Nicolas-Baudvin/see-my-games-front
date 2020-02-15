0a42b741de48c3e0b55f2655a626f5f58bf13ee3
Nicolas <nicolas.baudvin@gmail.com>
2020-02-15 15:25:55 +0100

    clean console.log

--- src/store/middleware.js
+++ src/store/middleware.js
@@ -1,11 +1,4 @@
 export default store => next => action => {
-  console.log('middleware', action);
-
-  // switch(action.type) {
-  //   case UNE_ACTION: {
-  //     // ...
-  //   }
-  // }
 
   next(action);
 };
