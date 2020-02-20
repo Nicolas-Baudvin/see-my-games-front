5667bc1ccd3898099d09b65b6c40a8999154f84c
Nicolas <nicolas.baudvin@gmail.com>
2020-02-20 17:06:03 +0100

    set up channels & list of connected users (wip)

--- src/components/ChatRoom/index.js
+++ src/components/ChatRoom/index.js
@@ -8,7 +8,7 @@ import 'emoji-mart/css/emoji-mart.css';
 import { Picker, Emoji, emojiIndex } from 'emoji-mart';
 import { chatChannels } from 'src/data/navs';
 import { useDispatch, useSelector } from 'react-redux';
-import { connectToChat, sendMessage } from '../../store/ChatRoom/actions';
+import { connectToChatGeneral, sendMessage, connectToChatSteam, connectToChatOther, disconnectFromChat, sendMessageOther, sendMessageSteam } from '../../store/ChatRoom/actions';
 
 export default () => {
   const [nav, setNav] = useState(chatChannels);
@@ -18,7 +18,7 @@ export default () => {
   const [searchEmojis, setSearchEmojis] = useState('grinning');
   const [msgArray, setMsgArray] = useState([]);
   const dispatch = useDispatch();
-  const { messages, steamMessages, otherMessages } = useSelector((state) => state.chat);
+  const { messages, steamMessages, otherMessages, usersConnectedSteam, usersConnectedOther, usersConnectedGeneral } = useSelector((state) => state.chat);
   const { userData } = useSelector((state) => state.user);
 
   const changeChan = (chanName) => (e) => {
@@ -36,9 +36,30 @@ export default () => {
   };
 
   const handleSubmitMessage = () => {
-    if (text) {
-      dispatch(sendMessage(text));
+    switch (currentChan) {
+      case "Général": {
+        if (text) {
+          dispatch(sendMessage(text));
+        }
+        break;
+      }
+      case "Autre": {
+        if (text) {
+          dispatch(sendMessageOther(text));
+        }
+        break;
+      }
+      case "Steam": {
+        if (text) {
+          dispatch(sendMessageSteam(text));
+        }
+        break;
+      }
+      default: {
+        break;
+      }
     }
+    setText('');
   };
 
   const handleClickEmoji = (emoji) => {
@@ -53,13 +74,13 @@ export default () => {
     const { target } = e;
     const msg = target.value;
     setText(msg);
-    console.log(msg.charAt(msg.length - 2));
     if (msg.substring(msg.length - 1) === ":" && msg.charAt(msg.length - 2) !== " ") {
       const emoji = emojiIndex.search(msgArray[1]).map((o) => o.native);
       setText(text.replace(`:${msgArray[1].replace(' ', '')}`, emoji[0]));
       setMsgArray([]);
     }
     else {
+      // eslint-disable-next-line no-lonely-if
       if (text.includes(':')) {
         setMsgArray(msg.split(':'));
         setSearchEmojis(msgArray[1]);
@@ -69,8 +90,33 @@ export default () => {
   };
 
   useEffect(() => {
-    dispatch(connectToChat());
-  }, []);
+    console.log(currentChan);
+    switch (currentChan) {
+      case "Général": {
+        dispatch(connectToChatGeneral());
+        // dispatch(disconnectFromChat("Steam"));
+        // dispatch(disconnectFromChat("Other"));
+        break;
+      }
+      case "Steam": {
+        dispatch(connectToChatSteam());
+        // dispatch(disconnectFromChat("Other"));
+        // dispatch(disconnectFromChat("Général"));
+        break;
+      }
+      case "Autre": {
+        dispatch(connectToChatOther());
+        // dispatch(disconnectFromChat("Général"));
+        // dispatch(disconnectFromChat("Steam"));
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }, [currentChan]);
+
+
 
   return (<div className="chatroom">
     <div className="chatroom-channels">
@@ -116,7 +162,33 @@ export default () => {
         {/* TODO: discussion ici */}
         {
           currentChan === "Général" && messages && messages.map((mes) => (
-            <div key={mes.message} className={ClassNames("message", { me: mes.user === userData.username })}>
+            <div key={mes._id} className={ClassNames("message", { me: mes.user === userData.username })}>
+              <div className="message-body">
+                <img className="message-img" src={mes.avatar ? mes.avatar : "../src/assets/default-avatar.png"} alt="avatar" />
+                <p className="message-text">
+                  {mes.message}
+                  <span className="message-date"><time>{mes.time}</time> par <span onClick={playerProfil} className="message-user">{mes.user}</span></span>
+                </p>
+              </div>
+            </div>
+          ))
+        }
+        {
+          currentChan === "Steam" && steamMessages && steamMessages.map((mes) => (
+            <div key={mes._id} className={ClassNames("message", { me: mes.user === userData.username })}>
+              <div className="message-body">
+                <img className="message-img" src={mes.avatar ? mes.avatar : "../src/assets/default-avatar.png"} alt="avatar" />
+                <p className="message-text">
+                  {mes.message}
+                  <span className="message-date"><time>{mes.time}</time> par <span onClick={playerProfil} className="message-user">{mes.user}</span></span>
+                </p>
+              </div>
+            </div>
+          ))
+        }
+        {
+          currentChan === "Autre" && otherMessages && otherMessages.map((mes) => (
+            <div key={mes._id} className={ClassNames("message", { me: mes.user === userData.username })}>
               <div className="message-body">
                 <img className="message-img" src={mes.avatar ? mes.avatar : "../src/assets/default-avatar.png"} alt="avatar" />
                 <p className="message-text">
@@ -132,6 +204,9 @@ export default () => {
         }
         {
           currentChan === "Autre" && !otherMessages.length && <h2 className="nomess">Il n'y a pas encore de message dans ce channel <Emoji emoji="cry" size={32} /></h2>
+        }
+        {
+          currentChan === "Général" && !messages.length && <h2 className="nomess">Il n'y a pas encore de message dans ce channel <Emoji emoji="cry" size={32} /></h2>
         }
       </div>
 
@@ -155,11 +230,21 @@ export default () => {
       </div>
 
     </div>
-    <div className="chatroom-friends">
-      <div className="chatroom-friends-header">
-        <h2 className="chatroom-friends-title">
+    <div className="chatroom-users">
+      <div className="chatroom-users-header">
+        <h2 className="chatroom-users-title">
           <Icon name="users" size="big" />
-          Vos amis
+          Utilisateurs en ligne
+          {
+            currentChan === "Général" && usersConnectedGeneral.length && usersConnectedGeneral.map((user) => {
+              return (
+                <div key={user.username} className="chatroom-users-user">
+                  <img className="chatroom-users-user-avatar" src={user.avatar} alt="avatar" />
+                  <h2 className="chatroom-users-user-username"> {user.username} </h2>
+                </div>
+              );
+            })
+          }
         </h2>
       </div>
     </div>
